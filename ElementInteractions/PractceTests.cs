076a734665225b36d9d952e8336b2bf4d90d4fa1
Nithin <nithinkv@live.com>
2018-05-17 19:09:32 +0530

    17052018

---
+++ ElementInteractions/PractceTests.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ElementInteractions
+{
+    [TestClass]
+    public class TestScenarios
+{
+
+        static IWebDriver Driver;
+        public IWebElement Table { get; private set; }
+        private  int columnIndex = 0, rowIndex = 0;
+
+        [TestInitialize]
+
+    public void BrowserInitialise()
+    {
+            Driver = new ChromeDriver();   
+
+    }
+
+    [TestMethod]
+    [TestCategory("Navigation")]
+
+    public void PracticeTest()
+    {
+            //Driver.Navigate().GoToUrl("http://www.ultimateqa.com");
+            //Driver.Manage().Window.Maximize();
+
+
+            //Assert.AreEqual("Home - Ultimate QA", Driver.Title);
+
+            //Driver.Navigate().GoToUrl("https://www.ultimateqa.com/automation");
+            //Assert.AreEqual("Automation Practice - Ultimate QA", Driver.Title);
+
+            //Driver.FindElement(By.XPath("//*[@href='../complicated-page']")).Click();
+
+            //Assert.AreEqual("Complicated Page - Ultimate QA", Driver.Title);
+
+            //Driver.Navigate().Back();
+
+            //Assert.AreEqual("Automation Practice - Ultimate QA",Driver.Title);
+
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/simple-html-elements-for-automation");
+
+            Table = Driver.FindElement(By.Id("htmlTableId"));
+
+            IList<IWebElement> htmlTableTr = Table.FindElements(By.XPath("//table[@id='htmlTableId']/tbody/tr"));
+            var columnCounter = 1;
+
+            foreach (var itemtr in htmlTableTr)
+            {
+                var tr = itemtr;
+                IList<IWebElement> htmlTableTd = tr.FindElements(By.XPath("./*"));
+
+                foreach (var itemtd in htmlTableTd)
+                {
+                    var td = itemtd;
+                    if(td.Text == "Automation Testing Architect")
+                    {
+                         columnIndex = columnCounter;
+                 
+                    }
+
+                    if(td.Text == "Salary")
+                    {
+
+                         rowIndex = htmlTableTd.IndexOf(itemtd) + 1;
+
+                    }
+                }
+                columnCounter++;
+            }
+
+            var locator = string.Format("//table[@id='htmlTableId']/tbody/tr[{0}]/td[{1}]", rowIndex, columnIndex);
+            var Salary = Table.FindElement(By.XPath(locator)).Text;
+        }
+
+
+       [TestMethod]
+
+    public void Manipulation()
+
+    {
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/filling-out-forms/");
+
+            Driver.FindElement(By.Id("et_pb_contact_name_1")).SendKeys("Name");
+            Driver.FindElement(By.Id("et_pb_contact_message_1")).SendKeys("Text Message");
+            Driver.FindElement(By.CssSelector("button.et_pb_contact_submit:nth-child(1)")).Submit();
+
+
+        }
+
+        [TestCleanup]
+
+        public void TestCleanUp()
+        {
+            Driver.Close();
+            Driver.Quit();
+
+        }
+}
+
+}
